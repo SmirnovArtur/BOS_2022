a2c94315508a1d0b75df1dd51b1df4f925a88744
SmirnovArtur <91366222+SmirnovArtur@users.noreply.github.com>
2022-12-24 01:15:33 +0300

    Update 1.11.sh

--- scripts/1.11.sh
+++ scripts/1.11.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 echo "Каталоги:"
-ls -al |grep ^d
-echo -e "\nОбычные файлы:"
-ls -al | grep ^-
-echo -e "\nСимвольные ссылки:"
-ls -al | grep ^l
-echo -e "\nСимвольные устройства:"
-ls -al | grep ^c
-echo -e "\nБлочные устройства:"
-ls -al | grep ^b
+ls -l |grep ^d
+echo -e "Обычные файлы:"
+ls -l | grep ^-
+echo -e "Символьные ссылки:"
+ls -l | grep ^l
+echo -e "Символьные устройства:"
+ls -l | grep ^c
+echo -e "Блочные устройства:"
+ls -l | grep ^b
